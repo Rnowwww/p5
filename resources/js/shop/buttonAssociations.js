cd1e7d346ad87b5b426535cd4358667179b32099
Rnowwww <37271630+Rnowwww@users.noreply.github.com>
2023-03-22 23:39:14 +0100

    Update buttonAssociations.js

--- resources/js/shop/buttonAssociations.js
+++ resources/js/shop/buttonAssociations.js
@@ -1,12 +1,6 @@
 
 
 // Store
-facebookStr.addEventListener("click", buyFacebook);
-twitterStr.addEventListener("click", buyTwitter);
-uberEatsStr.addEventListener("click", buyUberEats);
-quizzStr.addEventListener("click", buyQuizz);
-
-
 
 
 
